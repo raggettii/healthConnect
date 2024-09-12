e4969eec76a06324bb3e5f6b3b83aa4f4a41d339
raggettii <22bsm029@iiitdmj.ac.in>
2024-09-12 18:55:24 +0530

    implement BE logic for OTP verification

---
+++ app/api/verify-phone-number/send-otp/route.ts
@@ -0,0 +1,52 @@
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { options } from "../../auth/[...nextauth]/options";
+import twilio from "twilio";
+import toast from "react-hot-toast";
+
+const accountSID = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const serviceId = process.env.TWILIO_SERVICE_ID_OTP!;
+
+const client = twilio(accountSID, authToken);
+
+export default async function GET(req: NextRequest) {
+  console.log("HII from send-otp route");
+  const sessionData = await getServerSession(options);
+  const phoneNumber = sessionData?.user.phoneNumber!;
+  try {
+    if (phoneNumber) {
+      throw new Error();
+    }
+    const twilioResponse = await client.verify.v2
+      .services(serviceId)
+      .verifications.create({
+        channel: "sms",
+        to: phoneNumber,
+      });
+    if (twilioResponse.status === "pending") {
+      toast.success(`OTP successfully sent to ${phoneNumber}`);
+      return NextResponse.json(
+        {
+          sent: true,
+        },
+        { status: 200 }
+      );
+    } else {
+      toast.error(
+        `Please check your phone number ${phoneNumber}, or try again`
+      );
+      return NextResponse.json(
+        {
+          sent: false,
+        },
+        { status: 500 }
+      );
+    }
+
+    console.log("Hello");
+  } catch (error) {
+    console.error(`Error occured while sending OTP`);
+    return NextResponse.json({ message: "Error Occured" }, { status: 500 });
+  }
+}
---
+++ app/api/verify-phone-number/verify-otp/route.ts
@@ -0,0 +1,72 @@
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { options } from "../../auth/[...nextauth]/options";
+import twilio from "twilio";
+import toast from "react-hot-toast";
+import { PrismaClient } from "@prisma/client";
+
+const accountSID = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const serviceId = process.env.TWILIO_SERVICE_ID_OTP!;
+
+const client = twilio(accountSID, authToken);
+
+export default async function GET(req: NextRequest) {
+  const prisma = new PrismaClient();
+  const sessionData = await getServerSession(options);
+  const phoneNumber = sessionData?.user.phoneNumber!;
+  // const isVerified = sessionData?.user
+
+  const userId = sessionData?.user.id!;
+  const role = sessionData?.user.role;
+  try {
+    if (phoneNumber) {
+      throw new Error();
+    }
+
+    const { searchParams } = new URL(req.url);
+    const otp = searchParams.get("otp") as string;
+
+    const twilioResponse = await client.verify.v2
+      .services(serviceId)
+      .verificationChecks.create({
+        code: otp,
+        to: phoneNumber,
+      });
+
+    if (twilioResponse.status === "approved") {
+      if (role === "user") {
+        await prisma.healthConnect_User.update({
+          where: { id: userId },
+          data: { isVerified: true },
+        });
+      } else {
+        await prisma.healthConnect_Hospital.update({
+          where: { id: userId },
+          data: { isVerified: true },
+        });
+      }
+      // await prisma
+      toast.success(`OTP verified successfully`);
+      return NextResponse.json(
+        {
+          verified: true,
+        },
+        { status: 200 }
+      );
+    } else {
+      toast.error(`Enter correct OTP or try again`);
+      return NextResponse.json(
+        {
+          verified: false,
+        },
+        { status: 500 }
+      );
+    }
+
+    console.log("Hello");
+  } catch (error) {
+    console.error(`Error occured while sending OTP`);
+    return NextResponse.json({ message: "Error Occured" }, { status: 500 });
+  }
+}
